d167bcbb0de89b4135c5e6d9f2a984567c50c408
Viktor Leonov <v.leonov@mediasoft.team>
2020-02-03 12:46:10 +0400

    Making items snap to the center (#1 variant)

--- TestInfiniteScroll/Controller/ViewController.swift
+++ TestInfiniteScroll/Controller/ViewController.swift
@@ -16,8 +16,6 @@ class ViewController: UIViewController {
     let bannersNumber = 9
     let bannerImages = [#imageLiteral(resourceName: "Image11"), #imageLiteral(resourceName: "Image12"), #imageLiteral(resourceName: "Image7"), #imageLiteral(resourceName: "Image10"), #imageLiteral(resourceName: "Image8"), #imageLiteral(resourceName: "Image6"), #imageLiteral(resourceName: "Image4"), #imageLiteral(resourceName: "Image5")]
     
-    //https://picsum.photos/id/237/200/300
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,27 +23,28 @@ class ViewController: UIViewController {
         collectionView.dataSource = self
         
         self.collectionView.register(UINib(nibName: "BannerCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "BannerCollectionViewCell")
-        
+
+       
         collectionView.reloadData()
     }
-
-
+ 
 }
 
-extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return bannersNumber
+        return bannerImages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let bannerCell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCollectionViewCell", for: indexPath) as? BannerCollectionViewCell
         
-        let imageUrl = URL(string: "https://picsum.photos/id/\(indexPath.row)/900/1000")
-        let placeholderImgage = #imageLiteral(resourceName: "Placeholder")
-        bannerCell?.imageView.kf.setImage(with: imageUrl, placeholder: placeholderImgage)
-        bannerCell?.cellWidth.constant = collectionView.bounds.height / 1.3
+//        let imageUrl = URL(string: "https://picsum.photos/id/\(indexPath.row)/900/1000")
+//        let placeholderImgage = #imageLiteral(resourceName: "Placeholder")
+        bannerCell?.imageView.image = bannerImages[indexPath.item]
+//        bannerCell?.imageView.kf.setImage(with: imageUrl, placeholder: placeholderImgage)
+//        bannerCell?.cellWidth.constant = collectionView.bounds.height / 1.3
         
         return bannerCell ?? UICollectionViewCell()
 
@@ -53,3 +52,31 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     
 }
 
+extension ViewController: UICollectionViewDelegate {
+
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidth = collectionView.frame.width*0.8
+        let cellHeight =  collectionView.frame.height - layout.sectionInset.top*2
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+
+    
+    
+//    - (void)collectionView:(UICollectionView *)collectionView didUpdateFocusInContext:(UICollectionViewFocusUpdateContext *)context withAnimationCoordinator:(UIFocusAnimationCoordinator *)coordinator
+//    {
+//        // As we are not using the default scrollable feature from the UIScrollView,
+//        // we can scroll ourself to the center of the focused cell
+//        if (context.nextFocusedIndexPath && !collectionView.scrollEnabled) {
+//
+//          [collectionView scrollToItemAtIndexPath:context.nextFocusedIndexPath
+//                                 atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally
+//                                         animated:YES];
+//        }
+
+}
--- TestInfiniteScroll/Others/CircularCollectionViewLayout.swift
+++ TestInfiniteScroll/Others/CircularCollectionViewLayout.swift
@@ -11,14 +11,13 @@ import UIKit
 class CircularCollectionViewLayout: UICollectionViewFlowLayout {
     
     override func prepare() {
-        let minLineSpacing = self.minimumLineSpacing
         let contentOffset = self.collectionView?.contentOffset ?? CGPoint()
         let contentSize = super.collectionViewContentSize
         
-        let rightContentEdge =  contentSize.width + minLineSpacing
+        let rightContentEdge =  contentSize.width + minimumLineSpacing
         let leftContentEdge = CGFloat(0.0)
         let topContentEdge = CGFloat(0.0)
-        let bottomContentEdge = contentSize.height + minLineSpacing
+        let bottomContentEdge = contentSize.height + minimumLineSpacing
         
         // Changing collectionView offset after reaching its content edges
         switch self.scrollDirection {
@@ -41,16 +40,48 @@ class CircularCollectionViewLayout: UICollectionViewFlowLayout {
     }
     
     override var collectionViewContentSize: CGSize {
-        let minLineSpacing = self.minimumLineSpacing
         let contentSize = super.collectionViewContentSize
         let collectionViewSize = self.collectionView?.bounds.size ?? CGSize()
         
         // Adding collectionView's height/width to the contentSize to allow changing offset unnoticeably without any screen artifacts
         switch self.scrollDirection {
-        case .vertical: return CGSize(width: contentSize.width, height: contentSize.height + collectionViewSize.height + minLineSpacing)
-        case .horizontal: return CGSize(width: contentSize.width + collectionViewSize.width + minLineSpacing, height: contentSize.height)
+        case .vertical: return CGSize(width: contentSize.width, height: contentSize.height + collectionViewSize.height + minimumLineSpacing)
+        case .horizontal: return CGSize(width: contentSize.width + collectionViewSize.width + minimumInteritemSpacing, height: contentSize.height)
         @unknown default: fatalError("Correct collectionViewContentSize method for CircularCollectionViewLayout")
         }
+    }
+    
+    override open func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let contentSize = super.collectionViewContentSize
+        
+        var layoutAttributes = super.layoutAttributesForElements(in: rect) ?? []
+        
+        switch self.scrollDirection {
+        case .vertical:
+            let newRect = CGRect(x: rect.origin.x, y: rect.origin.y - contentSize.height, width: rect.width, height: rect.height)
+            if let wrappingAttributes = super.layoutAttributesForElements(in: newRect) {
+                wrappingAttributes.forEach{ $0.center.y += contentSize.height + minimumLineSpacing }
+                layoutAttributes += wrappingAttributes
+            }
+        case .horizontal:
+            let newRect = CGRect(x: rect.origin.x - contentSize.width, y: rect.origin.y, width: rect.width, height: rect.height)
+            if let wrappingAttributes = super.layoutAttributesForElements(in: newRect){
+                wrappingAttributes.forEach{ $0.center.x += contentSize.width + minimumLineSpacing }
+                layoutAttributes += wrappingAttributes
+            }
+        @unknown default: fatalError("Correct generateLayoutAttributes method for CircularCollectionViewLayout")
+        }
+        
+        return layoutAttributes
+    }
+    
+    open override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        let collectionViewSize = self.collectionView?.bounds.size ?? CGSize()
+        
+        let layoutAttributes = super.layoutAttributesForItem(at: indexPath)
+        layoutAttributes?.center.x += collectionViewSize.width
+        
+        return layoutAttributes
     }
     
     override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
@@ -68,42 +99,51 @@ class CircularCollectionViewLayout: UICollectionViewFlowLayout {
             }
         @unknown default: fatalError("Correct shouldInvalidateLayout method for CircularCollectionViewLayout")
         }
-    
+
         return super.shouldInvalidateLayout(forBoundsChange: newBounds)
     }
     
-    override open func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        let minLineSpacing = self.minimumLineSpacing
-        let contentSize = super.collectionViewContentSize
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
         
-        var layoutAttributes = super.layoutAttributesForElements(in: rect) ?? []
+        let layoutAttributes = layoutAttributesForElements(in: collectionView!.bounds)
+        
+        var targetContentOffset = CGPoint()
         
         switch self.scrollDirection {
         case .vertical:
-            let newRect = CGRect(x: rect.origin.x, y: rect.origin.y - contentSize.height, width: rect.width, height: rect.height)
-            if let wrappingAttributes = super.layoutAttributesForElements(in: newRect) {
-                wrappingAttributes.forEach{ $0.center.y += contentSize.height + minLineSpacing }
-                layoutAttributes += wrappingAttributes
-            }
+            let center = collectionView!.bounds.height / 2
+            let proposedCentrationOffset = proposedContentOffset.y + center
+            
+            let closest = layoutAttributes!.sorted{ abs($0.center.y - proposedCentrationOffset) < abs($1.center.y - proposedCentrationOffset) }.first ?? UICollectionViewLayoutAttributes()
+            targetContentOffset = CGPoint(x: proposedContentOffset.x, y:  floor(closest.center.y - center))
+        
         case .horizontal:
-            let newRect = CGRect(x: rect.origin.x - contentSize.width, y: rect.origin.y, width: rect.width, height: rect.height)
-            if let wrappingAttributes = super.layoutAttributesForElements(in: newRect){
-                wrappingAttributes.forEach{ $0.center.x += contentSize.width + minLineSpacing }
-                layoutAttributes += wrappingAttributes
-            }
-        @unknown default: fatalError("Correct generateLayoutAttributes method for CircularCollectionViewLayout")
+            let collectionCenter = collectionView!.bounds.width / 2
+            let proposedCentrationOffset = proposedContentOffset.x + collectionCenter
+            
+            let closest = layoutAttributes!.sorted{ abs($0.center.x - proposedCentrationOffset) < abs($1.center.x - proposedCentrationOffset) }.first ?? UICollectionViewLayoutAttributes()
+            
+            let stopPosition = floor(closest.center.x - collectionCenter)
+            
+            
+//            let one = layoutAttributes!.sorted{ abs($0.center.x - proposedCentrationOffset) > abs($1.center.x - proposedCentrationOffset) }.first ?? UICollectionViewLayoutAttributes()
+//            print("one: \(one.center.x - collectionCenter)")
+//
+//            targetContentOffset = CGPoint(x: floor(closest.center.x - center), y: proposedContentOffset.y)
+//            if floor(closest.center.x - center) < 0 {
+//                targetContentOffset = CGPoint(x: one.center.x - center, y: proposedContentOffset.y)
+//            } else {
+//                targetContentOffset = CGPoint(x: floor(closest.center.x - center), y: proposedContentOffset.y)
+//            }
+            
+            targetContentOffset = CGPoint(x: stopPosition, y: proposedContentOffset.y)
+            
+        @unknown default: fatalError("Correct targetContentOffset method for CircularCollectionViewLayout")
         }
         
-        return layoutAttributes
+        return targetContentOffset
     }
+
     
-    open override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        let collectionViewSize = self.collectionView?.bounds.size ?? CGSize()
-        
-        let layoutAttributes = super.layoutAttributesForItem(at: indexPath)
-        layoutAttributes?.center.x += collectionViewSize.width
-        
-        return layoutAttributes
-    }
 
 }
